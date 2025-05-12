23ffdcccc80fd79d1499dc83becc6c7a34b4bafa
tsuchidamay <mayara.tsuchida@escola.pr.gov.br>
2025-05-12 08:51:15 -0300

    commit1

---
+++ pages/Mayara/script.js
@@ -0,0 +1,13 @@
+const body = document.body;
+let angle = 270;
+let direction = 1;
+function animateGradient() {
+  // Modifica o ângulo de 270 a 450 graus e volta para gerar movimento diferente
+  angle += direction * 0.1;
+  if(angle > 450) direction = -1;
+  else if(angle < 270) direction = 1;
+  body.style.background = `linear-gradient(${angle}deg, #ff6a00, #ee0979, #ff6a00, #ee0979)`;
+  body.style.backgroundSize = '800% 800%';
+  requestAnimationFrame(animateGradient);
+}
+animateGradient();
