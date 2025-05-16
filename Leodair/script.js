1db1dbbe135631d29ab70054079b207e829a8421
Bryan Kauan <165050307+braiatenebras@users.noreply.github.com>
2025-05-16 07:31:42 -0300

    Delete Leodair directory

--- Leodair/script.js
+++
@@ -1,8 +0,0 @@
-function search() {
-    const query = document.getElementById("searchInput").value;
-    if (query) {
-      alert(`Você está buscando por: ${query}`);
-    } else {
-      alert("Por favor, insira um termo para buscar.");
-    }
-  }
