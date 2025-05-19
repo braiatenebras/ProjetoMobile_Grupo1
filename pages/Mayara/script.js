9a8d6958d8a8c1016153b77e142ddf25da8b18bd
tsuchidamay <mayara.tsuchida@escola.pr.gov.br>
2025-05-19 08:08:45 -0300

    botaofuncionando

--- pages/Mayara/script.js
+++ pages/Mayara/script.js
@@ -2,23 +2,29 @@ const body = document.body;
 let angle = 270;
 let direction = 1;
 function animateGradient() {
-  // Modifica o ângulo de 270 a 450 graus e volta para gerar movimento diferente
   angle += direction * 0.1;
   if(angle > 450) direction = -1;
   else if(angle < 270) direction = 1;
-  body.style.background = `linear-gradient(${angle}deg, #ff6a00, #ee0979, #ff6a00, #ee0979)`;
+  body.style.background = `linear-gradient(${angle}deg, #0f3460, #4B0082, #0f3460, #4B0082)`;
   body.style.backgroundSize = '800% 800%';
   requestAnimationFrame(animateGradient);
 }
 animateGradient();
 
-const button = document.querySelector('.play-pause-btn');
-    button.addEventListener('click', () => {
-      if (button.classList.contains('paused')) {
-        button.classList.remove('paused');
-        button.classList.add('playing');
-      } else {
-        button.classList.remove('playing');
-        button.classList.add('paused');
-      }
-    });
+const playPauseButton = document.querySelector('.play-pause-btn');
+
+playPauseButton.addEventListener('click', () => {
+    playPauseButton.classList.toggle('paused');
+
+    
+    const playIcon = playPauseButton.querySelector('.bi-play-fill');
+    const pauseIcon = playPauseButton.querySelector('.bi-pause-fill');
+
+    if (playPauseButton.classList.contains('paused')) {
+        playIcon.style.display = 'block';
+        pauseIcon.style.display = 'none';
+    } else {
+        playIcon.style.display = 'none';
+        pauseIcon.style.display = 'block';
+    }
+});
